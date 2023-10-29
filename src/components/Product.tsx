94e78e87caa2a88d761b9cf644db7fede5599bfb
Saboor Hussain <hussainsaboor456@gmail.com>
2023-10-29 14:29:15 +0500

    products ui added

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,18 +1,22 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
-interface ProductProps{
+interface ProductProps {
   product: Product;
 }
 
-export const Product:React.FC <ProductProps>= ({ product }) => {
+export const Product: React.FC<ProductProps> = ({ product }) => {
   return (
-    <div className='border rounded-lg p4 shadow-md'>
-      <h2 className='text-lg font-semibold'>{product.name}</h2>
-      <p className='text-gray-400'>${product.price.toFixed(2)}</p>
-      <button onClick={() => {}} disabled={false} className='rounded focus:outline-none focus:ring2 focus:ring-blue-200 focus:ring-offset-2'>
-        {true ? "Added to Cart": "Add to Cart"}
+    <div className="border rounded-lg p-4 shadow-md">
+      <h2 className="text-lg font-semibold">{product.name}</h2>
+      <p className="text-gray-400">${product.price.toFixed(2)}</p>
+      <button
+        onClick={() => {}}
+        disabled={false}
+        className="mt-2 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+      >
+        {true ? "Added to Cart" : "Add to Cart"}
       </button>
     </div>
-  )
-}
+  );
+};
