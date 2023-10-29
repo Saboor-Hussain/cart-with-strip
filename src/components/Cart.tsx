155fd3aea19db6e2bc3644d8c66d234b9e6754d7
Saboor Hussain <hussainsaboor456@gmail.com>
2023-10-29 15:32:45 +0500

    Cart Functionality is working now

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,66 +1,79 @@
 "use client";
+import { useCart } from "./CartContext";
 
 export default function Cart() {
+  const { cart, increamentQuantity, decrementQuantity, removeFromCart } =
+    useCart();
+
+  const totalAmount = cart.reduce(
+    (totalAmount, product) => totalAmount + product.price * product.quantity,
+    0
+  );
+
+  const handleDecrement = (productId: number) => {
+    decrementQuantity(productId);
+
+    const product = cart.find((item: Product) => item.id === productId);
+    if (product && product.quantity <= 1) {
+      removeFromCart(productId);
+    }
+  };
+
   return (
     <div className="border rounded-lg p-4 shadow-md">
       <h2 className="text-lg font-semibold mb-4 text-center"> Your Cart </h2>
-      <ul>
-        {/* Product 1 */}
-        <li className="flex justify-between items-center mb-2">
-          <div>
-            <p className="font-semibold">Product 1</p>
-            <p className="text-gray-400">$25.09 x 4</p>
-          </div>
-          <div className="flex space-x-2">
-            <button
-              onClick={() => {}}
-              className="px-2 py-1 text-white bg-red-600 hover:bg-red-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+      {cart.length === 0 ? (
+        <p className="text-gray-400 text-center">Your Cart is empty</p>
+      ) : (
+        <ul>
+          {cart.map((product) => (
+            <li
+              key={product.id}
+              className="flex justify-between items-center mb-2"
             >
-              -
-            </button>
+              <div>
+                <p className="font-semibold">{product.name}</p>
+                <p className="text-gray-400">
+                  ${product.price.toFixed(2)} x {product.quantity}
+                </p>
+              </div>
+              <div className="flex space-x-2">
+                <button
+                  onClick={() => handleDecrement(product.id)}
+                  className="px-2 py-1 bg-red-500 text-white hover:bg-red-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                >
+                  -
+                </button>
+                <button
+                  onClick={() => increamentQuantity(product.id)}
+                  className="px-2 py-1 bg-blue-500 text-white hover:bg-blue-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                >
+                  +
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
 
-            <button
-              onClick={() => {}}
-              className="px-2 py-1 text-white bg-blue-600 hover:bg-blue-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-            >
-              +
-            </button>
+      {/* Total Amount */}
+      {cart.length > 0 && (
+        <>
+          <div className="mt-4">
+            <p className="text-lg font-semibold">
+              Total Amount: ${totalAmount.toFixed(2)}
+            </p>
           </div>
-        </li>
 
-        {/* Product 2 */}
-        <li className="flex justify-between items-center mb-2">
-          <div>
-            <p className="font-semibold">Product 2</p>
-            <p className="text-gray-400">$56.60 x 3</p>
-          </div>
-          <div className="flex space-x-2">
-            <button
-              onClick={() => {}}
-              className="px-2 py-1 text-white bg-red-600 hover:bg-red-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-            >
-              -
-            </button>
-
-            <button
-              onClick={() => {}}
-              className="px-2 py-1 text-white bg-blue-600 hover:bg-blue-600 w-8 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-            >
-              +
-            </button>
-          </div>
-        </li>
-      </ul>
-
-      {/* Purchase Button */}
-      <div className="mt-4">
-        <p className="text-lg font-semibold">Total Amount: $234.123</p>
-      </div>
-      <button
-        onClick={() => {}}
-        className="mt-4 px-4 py-2 bg-green-500 text-white hover:bg-green-600 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"      >
-        Buy Now
-      </button>
+          {/* Strip Checkout Button */}
+          <button
+            onClick={() => {}}
+            className="mt-4 px-4 py-2 bg-green-500 text-white hover:bg-green-600 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+          >
+            Checkout
+          </button>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,21 +1,30 @@
 "use client";
 import React from "react";
+import { useCart } from "./CartContext";
 
 interface ProductProps {
   product: Product;
 }
 
 export const Product: React.FC<ProductProps> = ({ product }) => {
+  const { addToCart, cart } = useCart();
+
+  const isProductInCart = cart.some((item) => item.id === product.id);
+
   return (
     <div className="border rounded-lg p-4 shadow-md">
       <h2 className="text-lg font-semibold">{product.name}</h2>
       <p className="text-gray-400">${product.price.toFixed(2)}</p>
       <button
-        onClick={() => {}}
+        onClick={() => addToCart(product)}
         disabled={false}
-        className="mt-2 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+        className={`mt-2 px-4 py-2 ${
+          isProductInCart
+            ? "bg-gray-400 text-gray-600 cursor-not-allowed"
+            : "bg-blue-500 text-white hover:bg-blue-600"
+        } rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`}
       >
-        {true ? "Added to Cart" : "Add to Cart"}
+        {isProductInCart ? "Added to Cart" : "Add to Cart"}
       </button>
     </div>
   );
